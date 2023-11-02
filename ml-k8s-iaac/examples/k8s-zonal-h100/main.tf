0cd58465374eceaa9ed19fc1c5d53ce02fba4271
Boris Popov <malibora@gmail.com>
2023-11-02 16:40:06 +0100

    updated k8s module reference

--- ml-k8s-iaac/examples/k8s-zonal-h100/main.tf
+++ ml-k8s-iaac/examples/k8s-zonal-h100/main.tf
@@ -1,6 +1,6 @@
 
 module "kube" {
-  source = "github.com/nebius/terraform-nb-kubernetes.git?ref=1.0.0"
+  source = "github.com/nebius/terraform-nb-kubernetes.git?ref=1.0.1"
 
   network_id = "btcci5d99ka84l988qvs"
 
