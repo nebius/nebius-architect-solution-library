892b82d6ec69e1a0c146b2b0786d359bc26c829d
boris popov <malibora@gmail.com>
2023-12-07 12:56:33 +0100

    gpu operator chart change

--- ml-k8s-iaac/examples/k8s-zonal-h100-ib/helm.tf
+++ ml-k8s-iaac/examples/k8s-zonal-h100-ib/helm.tf
@@ -1,38 +1,13 @@
 data "nebius_client_config" "client" {}
 
-# resource "helm_release" "gpu_operator" {
-#   name       = "gpu-operator"   # Name of your Helm release
-#   chart      = "../../gpu-operator"
-#   namespace = "gpu-operator"
-#   create_namespace = true
-  # values = [
-  #   "${file("../../gpu-operator/values.yaml")}"
-  # ]
-
-#   set {
-#     name  = "toolkit.enabled"
-#     value = "true"
-#   }
-
-#   set {
-#     name  = "driver.rdma.enabled"
-#     value = "true"
-#   }
-
-#   set {
-#     name  = "driver.version"
-#     value = "535.104.12"
-#   }
-# }
-
-
 resource "helm_release" "gpu_operator" {
-  name       = "gpu-operator"
-  repository = "https://helm.ngc.nvidia.com/nvidia"
-  chart      = "gpu-operator"
-  version    = "v23.9.0"  # Specify the version you want to use
+  name       = "gpu-operator"   # Name of your Helm release
+  chart      = "../../gpu-operator"
   namespace = "gpu-operator"
   create_namespace = true
+  values = [
+    "${file("../../gpu-operator/values.yaml")}"
+  ]
 
   set {
     name  = "toolkit.enabled"
@@ -49,6 +24,31 @@ resource "helm_release" "gpu_operator" {
     value = "535.104.12"
   }
 }
+
+
+# resource "helm_release" "gpu_operator" {
+#   name       = "gpu-operator"
+#   repository = "https://helm.ngc.nvidia.com/nvidia"
+#   chart      = "gpu-operator"
+#   version    = "v23.9.0"  # Specify the version you want to use
+#   namespace = "gpu-operator"
+#   create_namespace = true
+
+#   set {
+#     name  = "toolkit.enabled"
+#     value = "true"
+#   }
+
+#   set {
+#     name  = "driver.rdma.enabled"
+#     value = "true"
+#   }
+
+#   set {
+#     name  = "driver.version"
+#     value = "535.104.12"
+#   }
+# }
 
 
 
